ce13c69af7bf592c46b5650d53d0318bf17d9b6a
yokarak <54890038+yokarak@users.noreply.github.com>
2020-10-17 21:17:35 +0200

    made a test route and a test for the route

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var path = require('path');
 var port = process.env.PORT || '8000'
 
 var homeRouter = require('./routes/home');
+var testerRouter = require('./routes/test');
 
 app.engine('handlebars', exhbs());
 app.set('view engine', 'handlebars');
@@ -13,9 +14,12 @@ app.set('view engine', 'handlebars');
 //app.use(express.static(path.join(__dirname, 'folder'))) not sure if i need to have a static "folder"
 
 app.use('/', homeRouter);
+app.use('/test', testerRouter);
 
 
 
 app.listen(port, () => {
     console.log('Server is starting at port', port);
 })
+
+module.exports = app;
---
+++ test/sendTest.js
@@ -0,0 +1,8 @@
+const test = require('supertest');
+const app = require('../server');
+
+describe("Get /", ()=> {
+    it('respond with Tester tester', (done) => {
+        test(app).get('/test').expect('Tester tester', done);
+    })
+});
